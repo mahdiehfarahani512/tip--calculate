acd732bcdc242225444811d8086a7db520e93786
mahdieh <mahdiefarahani00@gmail.com>
2025-09-18 20:16:16 +0330

    v2

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const billEl=document.getElementById("billamount");
-const tipEl=document.getElementById("tip");
-const calEl=document.getElementById("calculate");
-const totalEl=document.getElementById("total");
+const billEl = document.getElementById("billamount");
+const tipEl = document.getElementById("tip");
+const calEl = document.getElementById("calculate");
+const totalEl = document.getElementById("total");
 
-calEl.addEventListener("click",()=>(
-    const sum - Number(billEl.value)+ Number(tipEl.value);
-    totalEl.innerText=sum;
-))
+calEl.addEventListener("click", () => {
+  const sum = Number(billEl.value) + Number(tipEl.value);
+  totalEl.innerText = sum;
+});
 
-
-console.log(billEl,tipEl,calEl,totalEl);
-
-
+// console.log(billEl, tipEl, calEl, totalEl);
+console.log(calEl);
